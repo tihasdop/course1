e73a7fb1933149002ce913855b3a4a0dc921ac94
Alexander Tomilov <tihasdop@gmail.com>
2017-06-11 23:55:14 +0800

    Add licence for MiniZip files

--- crypt_minizip.h
+++ crypt_minizip.h
@@ -1,5 +1,5 @@
 /* crypt.h -- base code for crypt/uncrypt ZIPfile
-
+   part of the MiniZip project - ( http://www.winimage.com/zLibDll/minizip.html )
 
    Version 1.01e, February 12th, 2005
 
@@ -25,6 +25,29 @@
    The new AES encryption added on Zip format by Winzip (see the page
    http://www.winzip.com/aes_info.htm ) and PKWare PKZip 5.x Strong
    Encryption is not supported.
+*/
+
+/*
+----------------------------------------------------------
+   Condition of use and distribution are the same than zlib :
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+----------------------------------------------------------
 */
 
 #define CRC32(c, b) ((*(pcrc_32_tab+(((int)(c) ^ (b)) & 0xff))) ^ ((c) >> 8))
