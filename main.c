92606a3287a553b5a73da3c2967813f90a6996dc
Alexander Tomilov <tihasdop@gmail.com>
2017-06-12 00:18:22 +0800

    Add license in my files; cleaning

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+/*
+main.c -- is a part of the ziphack
+
+Copyright (c) 2017 Alexander Tomilov
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +34,6 @@
 
 #define PROGRAM_NAME "ziphack"
 
-//TODO: rewrite help
 char *help = "Usage:\n\t" PROGRAM_NAME " [-d <dictionary file>] [-P] <zip file>\n\n"
 	"\t-d -- use passwords dictionary\n"
 	"\t-P -- use all printed ASCII chars as possible in password\n";
